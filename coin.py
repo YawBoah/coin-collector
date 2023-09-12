e8e5d222e4ff1dc020443cdcd82e8e3bc057bbf9
YBoah <yawboahene.yb@gmail.com>
2023-09-12 19:12:24 +0000

    still trying the pyzero

--- coin.py
+++ coin.py
@@ -1,7 +1,24 @@
-width=400
+import pgzrun
 
+width=400
 height=400
 
 score=0
 
+game_over = False
 
+fox = Actor("fox")
+fox.pos = (200, 200)
+
+coin = Actor("coin")
+coin.pos = (200, 200)
+
+def update():
+    pass
+
+def draw():
+    screen.clear()
+    fox.draw()
+    coin.draw()
+
+pgzrun.go()
